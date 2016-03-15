46728bdb3ed5ba2619bb75ce42666d106d97d965
Mike Land <mikepland@gmail.com>
2016-03-15 16:58:14 -0500

    sets up picker test

--- anotherNewApp/ViewController.swift
+++ anotherNewApp/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    let pickerOptions:[Int] = [2,3,4,5]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.pickerOptions.count
+    }
 
-
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("I got picked")
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerOptions[row])
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
 }
 
--- anotherNewAppUITests/anotherNewAppUITests.swift
+++ anotherNewAppUITests/anotherNewAppUITests.swift
@@ -31,6 +31,9 @@ class anotherNewAppUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIApplication().pickerWheels["2"].adjustToPickerWheelValue("4")
+        
     }
     
 }
